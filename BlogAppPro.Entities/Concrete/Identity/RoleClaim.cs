44f62707670a1413ab09b390fd90653331074119
UmutUlupinar <umutulupinarrr@gmail.com>
2022-05-26 16:59:26 +0300

    .

--- BlogAppPro.Entities/Concrete/Identity/RoleClaim.cs
+++ BlogAppPro.Entities/Concrete/Identity/RoleClaim.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace BlogAppPro.Entities.Concrete.Identity
 {
-    public class RoleClaim
+    public class RoleClaim : IdentityRoleClaim<int>
     {
     }
+
 }
+
